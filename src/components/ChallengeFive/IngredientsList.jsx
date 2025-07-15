ac47dddae264f816a3fe522cc99e0980dacb28f4
Marion Jhon Agapay <marionagapay2001@gmail.com>
2025-07-15 19:55:45 +0800

    refactor: make another 2 file for recipe and ingredients list to make it cleaner

---
+++ src/components/ChallengeFive/IngredientsList.jsx
@@ -0,0 +1,26 @@
+const IngredientsList = (props) => {
+
+  const ingredientList = props.ingredients.map((ingredient) => {
+    return <li key={ingredient}>{ingredient}</li>;
+  });
+
+  return (
+    <section>
+      <h2>Ingredients on hand:</h2>
+      <ul className="ingredients-list" aria-live="polite">
+        {ingredientList}
+      </ul>
+      {props.ingredients.length > 3 && (
+        <div className="recipe-container">
+          <div>
+            <h3>Ready for a recipe?</h3>
+            <p>Generate a recipe from your list of ingredients</p>
+          </div>
+          <button onClick={props.getRecipe}>Get a recipe</button>
+        </div>
+      )}
+    </section>
+  );
+};
+
+export default IngredientsList;
--- src/components/ChallengeFive/Main.jsx
+++ src/components/ChallengeFive/Main.jsx
@@ -1,15 +1,23 @@
 import { useState } from "react";
+import ClaudeRecipe from "./ClaudeRecipe";
+import IngredientsList from "./IngredientsList";
 
 const Main = () => {
-  const [ingredients, setIngredients] = useState([]);
-
-  const ingredientList = ingredients.map((ingredient) => {
-    return <li key={ingredient}>{ingredient}</li>;
-  });
+  const [ingredients, setIngredients] = useState([
+    "all the main spices",
+    "pasta",
+    "ground beef",
+    "tomato paste",
+  ]);
+  const [recipeShown, setRecipeShown] = useState(false);
 
   const handleSubmit = (formData) => {
     const ingredient = formData.get("ingredient");
     setIngredients((prevIngredients) => [...prevIngredients, ingredient]);
+  };
+
+  const getRecipe = () => {
+    setRecipeShown((prevShow) => !prevShow);
   };
 
   return (
@@ -24,19 +32,8 @@ const Main = () => {
           />
           <button type="save">Add Ingredient</button>
         </form>
-        {ingredients.length > 0 &&<section>
-          <h2>Ingredients on hand:</h2>
-          <ul className="ingredients-list" aria-live="polite">
-            {ingredientList}
-          </ul>
-          {ingredients.length === 3 && <div className="recipe-container">
-            <div>
-              <h3>Ready for a recipe?</h3>
-              <p>Generate a recipe from your list of ingredients</p>
-            </div>
-            <button>Get a recipe</button>
-          </div>}
-        </section>}
+        {ingredients.length > 0 && <IngredientsList getRecipe={getRecipe} ingredients={ingredients}/>}
+        {recipeShown && <ClaudeRecipe />}
       </div>
     </main>
   );
